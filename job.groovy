6cb4a3e2487da8a2d0326d20e997c24f66e383ec
Imransysg <imranahmedm@sysgain.com>
2017-10-20 13:06:28 +0530

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -1,12 +1,17 @@
-node{
-    
-    docker.withRegistry('https://dtrnodednsitqp7hrth6jf2.westus.cloudapp.azure.com/', 'ddc-dtr-login') {
-        git 'https://github.com/SattaRavi/example-voting-app.git'
-        
-         build 'Job-Result'
-         build 'Job-Vote'
-         build 'Job-Worker'
-        
+node
+{
+    docker.withRegistry('https://10.1.53.4/','dtr-login'){
+       // dtr-login is a login ID in credentials 
+        stage "syncing files"
+        git 'https://github.com/sysgain/example-voting-app.git'
+
+        stage "Building and pushing Vote Image"
+        def vote_img = docker.build('dockeradmin/voting-app-vote','./vote').push('latest')
+
+        stage "Building and pushing Worker Image"
+        def worker_img = docker.build('dockeradmin/voting-app-worker','./worker').push('latest')
+
+        stage "Building and pushing Result Image"
+        def result_img = docker.build('dockeradmin/voting-app-result','./result').push('latest')
     }
-    
 }
